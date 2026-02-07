74e4ba77ed2bff48fec91925b74f995d1d171721
Jess Wagner <146011086+JessWagner805@users.noreply.github.com>
2026-02-07 12:12:01 -0500

    Add mock Jira issue data structure

---
+++ MockJira.py
@@ -0,0 +1,17 @@
+jira_issue = {
+    "issue_key": "PM-214",
+    "status_history": [
+        ("To Do", "2024-03-01"),
+        ("In Progress", "2024-03-02"),
+        ("Review", "2024-03-05"),
+        ("In Progress", "2024-03-06"),
+        ("Done", "2024-03-08"),
+        ("Reopened", "2024-03-09"),
+        ("In Progress", "2024-03-10")
+    ],
+    "story_points": 5,
+    "actual_days_in_progress": 9,
+    "team_avg_days": 4,
+    "comments_count": 14,
+    "times_reopened": 1
+}
